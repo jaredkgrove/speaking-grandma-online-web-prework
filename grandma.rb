6cbdbc472929f63932b4763b157f1501975456a4
Jared Grove <jaredkgrove@gmail.com>
2019-02-24 00:18:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
+<<<<<<< HEAD
     "I LOVE YOU TOO PUMPKIN!"
     elsif string == string.upcase
     "NO, NOT SINCE 1938!"
@@ -20,3 +21,14 @@ def speak_to_grandma(string)
     "HUH?! SPEAK UP, SONNY!"
   end
 end
+=======
+    puts "I LOVE YOU TOO PUMPKIN!"
+    elsif string == string.upcase
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
+puts "start"
+speak_to_grandma("hi")
+>>>>>>> a95d99c54ddf6f5ccba6546d5486809c80584044
